15fdd9922095959492d5ceaae0056ae8c55d8703
Dima Simagin <dimasimagin@outlook.com>
2022-02-06 17:50:20 +0100

    added copyright to menu

--- mainwindow.py
+++ mainwindow.py
@@ -5,7 +5,12 @@ from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QStatusBar
 from PyQt5.QtWidgets import QToolBar
+from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout
+from PyQt5.QtGui import QFont, QIcon, QPixmap
+from PyQt5.QtCore import Qt, QSize
 
+from widgets.mainlabel import MainLabel
 
 __version__ = '0.1'
 __author__ = 'Dmitrij Simagin'
@@ -15,16 +20,25 @@ class Window(QMainWindow):
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
-        self.setWindowTitle('QMainWindow')
-        self.setCentralWidget(QLabel("I'm the Central Widget"))
-        self.setMaximumSize(500,500)
+        self.x=100
+        self.y=100
+        self.width=1300
+        self.height=910
+        self.setGeometry(self.x,self.y,self.width,self.height)
+        self.setMinimumSize(self.width, self.height)
+        self.setStyleSheet("background:rgb(255,255,255)")
+        self.setWindowTitle('Test Case Import Tool')
+        welcomeText = MainLabel("Eximia Engineering\nTest Case Import Tool")
+        #self.setCentralWidget(welcomeText)
         self._createMenu()
         self._createToolBar()
         self._createStatusBar()
 
     def _createMenu(self):
         self.menu = self.menuBar().addMenu("&Menu")
-        self.menu.addAction('&Exit', self.close)
+        copyright = QAction("Copyright",self)
+        self.menu.addAction(copyright)
+        copyright.triggered.connect(self._open_copyright)
 
     def _createToolBar(self):
         tools = QToolBar()
@@ -35,6 +49,39 @@ class Window(QMainWindow):
         status = QStatusBar()
         status.showMessage("I'm the Status Bar")
         self.setStatusBar(status)
+
+    def _open_copyright(self):
+        Copyright=QDialog(None, Qt.WindowTitleHint | Qt.WindowCloseButtonHint)
+
+        OkBtn=QDialogButtonBox(Copyright)
+        #Copyright.accepted.connect(self.close())
+        Copyright.setFixedSize(600,500)
+        Copyright.setWindowTitle("Copyright")
+
+        # Copyright notice
+        CopyrightLbl=QLabel("Lizenzhinweis")
+        CopyrightLbl.setFont(QFont("Arial", 14, QFont.Bold))
+        CopyrightLbl1=QLabel("Copyright (c) 2022 Dmitrij Simagin")
+        CopyrightLbl2=QLabel('Permission is hereby granted, free of charge, to any person obtaining a copy of this software and\nassociated documentation files (the "Software"), to deal in the Software without restriction,\nincluding without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,\nand/or sell copies of the Software, and to permit persons to whom the Software is furnished to do\nso, subject to the following conditions:')
+        CopyrightLbl3=QLabel("The above copyright notice and this permission notice shall be included in all copies or\nsubstantial portions of the Software.")
+        CopyrightLbl4=QLabel('THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,\nINCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A\nPARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR\nCOPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN\nAN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION\nWITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.')
+
+        CopyrightLayout=QVBoxLayout()
+        CopyrightLayout.addWidget(CopyrightLbl)
+        CopyrightLayout.addSpacing(40)
+        CopyrightLayout.addWidget(CopyrightLbl1)
+        CopyrightLayout.addSpacing(40)
+        CopyrightLayout.addWidget(CopyrightLbl2)
+        CopyrightLayout.addSpacing(40)
+        CopyrightLayout.addWidget(CopyrightLbl3)
+        CopyrightLayout.addSpacing(40)
+        CopyrightLayout.addWidget(CopyrightLbl4)
+
+        # Set layout of Dialog-Window
+        Copyright.setLayout(CopyrightLayout)
+
+        # Execute Dialog-Window
+        Copyright.exec()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
