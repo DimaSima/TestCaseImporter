c095ec6d3e4412f6dcdddd12f44f7e95839f3726
Dima Simagin <dimasimagin@outlook.com>
2022-02-13 17:29:53 +0100

    need no commit

--- widgets/mainlabel.py
+++ widgets/mainlabel.py
@@ -1,15 +1,22 @@
 import sys
-from PyQt5.QtWidgets import QVBoxLayout,QMainWindow,QApplication,QLabel,QWidget
-from PyQt5.QtGui import QPixmap, QPalette
+from PyQt5.QtWidgets import QVBoxLayout,QLabel,QWidget, QHBoxLayout
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QIcon, QPixmap
 
 class MainLabel(QWidget):
     def __init__(self,text):
         super().__init__()
+        self.text = text
         self.initUI()
 
     def initUI(self):
-        label_ = QLabel(self)
-        label_.setText(self.text)
-        label_.setAlignment(Qt.AlignCenter)
-        
+        _label = QLabel(self)
+        _label.setText(self.text)
+        _label.setAlignment(Qt.AlignCenter)
+        _label.setFont(QFont("Arial", 14, QFont.Bold))
+
+        labelLayout=QHBoxLayout()
+        labelLayout.addWidget(_label)
+
+        self.setLayout(labelLayout)
+        self.show()
